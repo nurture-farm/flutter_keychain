d41a434a7ab9bc033d01e71987a1f3b4d34b9970
Rohit Lokhande <rohit.lokhande@neebal.com>
2021-01-08 09:59:00 +0530

    --add check for iOS platform in method channel call

--- lib/flutter_keychain.dart
+++ lib/flutter_keychain.dart
@@ -1,25 +1,45 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
 
 class FlutterKeychain {
-  static const MethodChannel _channel =
-      const MethodChannel('plugin.appmire.be/flutter_keychain');
+  static const MethodChannel _channel = const MethodChannel('plugin.appmire.be/flutter_keychain');
 
   // put - store the value for a key.
-  static Future<void> put(
-          {@required String key, @required String value}) async =>
-      _channel.invokeMethod('put', {'key': key, 'value': value});
+  static Future<void> put({@required String key, @required String value}) async {
+    try {
+      if (Platform.isIOS) {
+        await _channel.invokeMethod('put', {'key': key, 'value': value});
+      }
+    } catch (e) {}
+  }
 
   // get - get the value for a given key
-  static Future<String> get({@required String key}) async =>
-      await _channel.invokeMethod('get', {'key': key});
+  static Future<String> get({@required String key}) async {
+    try {
+      if (Platform.isIOS) {
+        await _channel.invokeMethod('get', {'key': key});
+      }
+    } catch (e) {}
+  }
 
   // remove - remove entry for a given key
-  static Future<void> remove({@required String key}) async =>
-      await _channel.invokeMethod('remove', {'key': key});
+  static Future<void> remove({@required String key}) async {
+    try {
+      if (Platform.isIOS) {
+        await _channel.invokeMethod('remove', {'key': key});
+      }
+    } catch (e) {}
+  }
 
   // clear - clear the keychain
-  static Future<void> clear() async => await _channel.invokeMethod('clear');
+  static Future<void> clear() async {
+    try {
+      if (Platform.isIOS) {
+        await _channel.invokeMethod('clear');
+      }
+    } catch (e) {}
+  }
 }
